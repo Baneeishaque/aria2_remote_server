11af161a9e34a54ebb4fdd95dfaf30a88dfa06a0
dk <dk@dk-PC>
2018-02-13 14:14:14 +0530

    last

--- http_API/select_Configuration.php
+++ http_API/select_Configuration.php
@@ -4,14 +4,13 @@ include_once 'config.php';
 
 $select_configuration_sql = "SELECT `system_status` FROM `configuration`";
 
-$result_array = array();
-
 if (!$select_configuration_result = $aria2_remote_connection->query($select_configuration_sql)) {
-    array_push($result_array, array("error_status" => "1", "error" => $aria2_remote_connection->error, "error_number" => $aria2_remote_connection->errorno));
+    $result_array = array("error_status" => "1", "error" => $aria2_remote_connection->error, "error_number" => $aria2_remote_connection->errorno);
 } else {
     if ($select_configuration_result->num_rows === 0) {
-        array_push($result_array, array("error_status" => "1", "error" => "Empty result set", "error_number" => "N/A"));
+        $result_array = array("error_status" => "2");
     } else {
+        $result_array = array();
         array_push($result_array, array("error_status" => "0"));
         $result_array[] = $select_configuration_result->fetch_assoc();
     }
