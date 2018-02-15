967e6c23cdd3196e399a87886ee47261c8aeea63
dk <dk@dk-PC>
2018-02-15 23:46:08 +0530

    update task fix

--- http_API/select_Tasks.php
+++ http_API/select_Tasks.php
@@ -8,15 +8,15 @@ $host = filter_input(INPUT_GET, 'host');
 //Status 0 : Waiting for addition
 $select_tasks_sql = "SELECT `id`, `url` FROM `tasks` WHERE `host_id`=(SELECT `id` FROM `hosts` WHERE `name`='$host') AND `status`=0";
 
-
+$result_array = array();
 
 if (!$select_tasks_result = $aria2_remote_connection->query($select_tasks_sql)) {
-    $result_array = array("error_status" => "1", "error_number" => $aria2_remote_connection->errorno, "error" => $aria2_remote_connection->error);
+    array_push($result_array, array("error_status" => "1", "error_number" => $aria2_remote_connection->errorno, "error" => $aria2_remote_connection->error));
 } else {
     if ($select_tasks_result->num_rows === 0) {
-        $result_array = array("error_status" => "2");
+        array_push($result_array, array("error_status" => "2"));
     } else {
-        $result_array = array();
+
         array_push($result_array, array("error_status" => "0"));
 
         while ($task_row = $select_tasks_result->fetch_assoc()) {
--- http_API/update_Task.php
+++ http_API/update_Task.php
@@ -8,6 +8,8 @@ $gid = filter_input(INPUT_POST, 'gid');
 //Status 1 : Added to downloads
 $update_task_sql = "UPDATE `tasks` SET `gid`='$gid',`status`=1,`modification_date_time`=CONVERT_TZ(NOW(),'-05:30','+00:00') WHERE `id`=$id";
 
+//echo $update_task_sql;
+
 if (!$aria2_remote_connection->query($update_task_sql)) {
     $result_array = array("error_status" => "1", "error_number" => $aria2_remote_connection->errorno, "error" => $aria2_remote_connection->error);
 } else {
